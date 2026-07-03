54229ba95cc49bd0b7c24ae8a4e80fde5981e96c
Madison Dunitz <7864111+MDunitz@users.noreply.github.com>
2026-07-03 01:07:47 -0700

    Refactor test_exploratory_constants_import
    Remove redundant import statement from test function.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -5,11 +5,10 @@ import os
 
 # Allow imports from repo root
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+from exploratory.constants import T, molar_mass_C
 
 
 def test_exploratory_constants_import():
-    from exploratory.constants import T, molar_mass_C
-
     # 0 degC in Kelvin
     assert T == 273.15
     # Carbon molar mass in g/mol
